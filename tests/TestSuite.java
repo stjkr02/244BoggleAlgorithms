e090f268299a0fbbd5227a6f3f59839d6e8a06e9
stjkr02 <stjkr02@moravian.edu>
2015-10-05 12:54:18 -0400

    Added another test, and a test suite to test all of my tests at once.

---
+++ tests/TestSuite.java
@@ -0,0 +1,12 @@
+import org.junit.runners.Suite;
+import org.junit.runner.RunWith;
+
+/**
+ * This fancy code creates a test suite for junit to run.  If you tell IntelliJ to run this
+ * class, it will run all of your tests.  Just be sure to add all your test files to the list!
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({LexiconALTest.class, LexiconTrieTest.class})
+public class TestSuite {
+    //nothing
+}
